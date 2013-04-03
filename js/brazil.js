b01890654d7a96bbc86eb1fd7bb50f6b7e6049f8
saleiva <saleiva@gmail.com>
2013-04-03 17:42:45 +0200

    improvements on the line chart rendering

--- js/brazil.js
+++ js/brazil.js
@@ -1,80 +1,86 @@
 var margin = 30;
-var h = 388;
+var top_margin = 140;
+var h = 380;
 var w = 1038;
 var subject = [];
 var svg = [];
 
 var LINE_DOT_R = 4;
-var STD_DOMAIN = [new Date('1980'),new Date('2012')];
-
 
 $(document).ready(function() {
-  for (var i = 0; i < datasets.sectors[0].subjects.length; i++) {
+
+  var _domainq = 'http://cpi.cartodb.com/api/v2/sql?q=SELECT%20min(min)%20as%20min,%20max(max)%20as%20max%20FROM%20(';
+
+  for (var i = 0; i < datasets.sectors[0].subjects.length; i++){
+    if(i!=0){
+      _domainq = _domainq + '%20UNION%20'
+    }
     subject[i] = datasets.sectors[0].subjects[i];
     svg[i] = d3.select("#"+subject[i].table).append("svg").attr("width", w).attr("height", h);
+    _domainq = _domainq + 'SELECT%20min(date_processed)%20as%20min,%20max(date_processed)%20as%20max%20FROM%20'+subject[i].table
   }
 
-  console.log(subject);
-  drawLineChart(0,STD_DOMAIN);
+  d3.json(_domainq+')%20as%20aux%20&api_key=eca1902cb724e40fdb20fd628b47489b15134d79', function(data){
+    var std_domain = [new Date(data.rows[0].min),new Date(data.rows[0].max)];
+    drawLineChart(0, std_domain);
+    drawLineChart(1, std_domain);
+    drawLineChart(2, std_domain);
+    drawLineChart(3, std_domain);
+  })
+
 
   function drawLineChart(index,domain){
 
     d3.json('http://cpi.cartodb.com/api/v2/sql?q=SELECT%20*%20FROM%20'+subject[index].table+"%20order%20by%20"+subject[index].x_axis+"%20&api_key=eca1902cb724e40fdb20fd628b47489b15134d79", function(data) {
-    
-    var _data = data;
+      
+      var _data = data;
+      if(domain === undefined){
+        _data['min_domain'] = d3.min(_data.rows, function(data){return data.date_processed;})
+        _data['max_domain'] = d3.max(_data.rows, function(data){return data.date_processed;})
+        _domain = [new Date(_data.min_domain),new Date(_data.max_domain)];
+      }else{
+        _domain = domain;
+      }
 
-      //AVOID UNNECESSARY CALLS WHEN DOMAIN IS DEFINED & FIX STD_DOMAIN
-      d3.json('http://cpi.cartodb.com/api/v2/sql?q=SELECT%20min(date_processed)%20as%20min,%20max(date_processed)%20as%20max%20FROM%20'+subject[index].table+'&api_key=eca1902cb724e40fdb20fd628b47489b15134d79', function(data) {
-        if(domain === undefined){
-          _domain = [new Date(data.rows[0].min),new Date(data.rows[0].max)];
-        }else{
-          _domain = domain;
-        }
+      var x_col = subject[index].x_axis;
+      var x_scale = d3.scale.linear()
+        .range([margin,w-margin])
+        .domain(_domain);
 
-        var x_col = subject[index].x_axis;
+      for (var i = 0; i < subject[index].series.length; i++) {
+        // append one group per series
+        var g = svg[index].append("svg:g");
+        var y_col = subject[index].series[i].column;
+        var srokeColor = subject[index].series[i].strokeColor;
 
-        var x_scale = d3.scale.linear()
-          .range([margin,w-margin])
-          .domain(_domain);
-          
+        var y_scale = d3.scale.linear()
+          .range([h-margin, top_margin])
+          .domain(subject[index].series[i].y_extent);
 
-        for (var i = 0; i < subject[index].series.length; i++) {
-          // append one group per series
-          var g = svg[index].append("svg:g");
-          var y_col = subject[index].series[i].column;
+        // remove null values
+        var data_col = _data.rows.filter(function(d) {
+          return (d[y_col] != null) && (d[y_col] != 0)
+        });
 
-          var y_scale = d3.scale.linear()
-            .range([h-margin, margin])
-            .domain(subject[index].series[i].y_extent);
+        var line = d3.svg.line()
+          .x(function(d){return x_scale(new Date(d[x_col]))})
+          .y(function(d){return y_scale(d[y_col])});
 
-          // remove null values
-          var data_col = _data.rows.filter(function(d) {
-            return (d[y_col] != null) && (d[y_col] != 0)
-          });
+        g.append("svg:path")
+          .attr("d", line(data_col))
+          .attr("class", 'lineStyle')
+          .attr("style",'stroke:'+srokeColor);
 
-          var line = d3.svg.line()
-            .x(function(d){return x_scale(new Date(d[x_col]))})
-            .y(function(d){return y_scale(d[y_col])});
-
-          g.append("svg:path")
-            .attr("d", line(data_col))
-            .attr("class", 'lineStyle');
-
-          g.selectAll("circle")
-            .data(data_col)
-            .enter()
-            .append("circle")
-            .attr("class", 'linedot')
-            .attr("cx", function(d){return x_scale(new Date(d[x_col]))})
-            .attr("cy", function(d){return y_scale(d[y_col])})
-            .attr("r", LINE_DOT_R);
-        }
-      });
+        g.selectAll("circle")
+          .data(data_col)
+          .enter()
+          .append("circle")
+          .attr("class", 'linedot')
+          .attr("style",'fill:'+srokeColor)
+          .attr("cx", function(d){return x_scale(new Date(d[x_col]))})
+          .attr("cy", function(d){return y_scale(d[y_col])})
+          .attr("r", LINE_DOT_R);
+      }
     });
   }
-
-  drawLineChart(1);
-  drawLineChart(2, STD_DOMAIN);
-  drawLineChart(3, STD_DOMAIN);
-
 });
