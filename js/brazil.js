44ad3f433b36362950ce772051f0dd3222a32889
Diego Cano <diegocano@Diegos-MacBook-Pro.local>
2013-04-04 11:57:37 +0200

    Basic version of area chart

--- js/brazil.js
+++ js/brazil.js
@@ -57,8 +57,104 @@ $(document).ready(function() {
     drawLineChart(0, std_domain);
     drawLineChart(1, std_domain);
     drawLineChart(2, std_domain);
-    drawLineChart(3, std_domain);
+    drawAreaChart(3, std_domain);
   })
+
+  function drawAreaChart(index, domain) {
+    console.log("Drawing area chart for "+index);
+
+    d3.json('http://cpi.cartodb.com/api/v2/sql?q=SELECT%20*%20FROM%20'+subject[index].table+"%20order%20by%20"+subject[index].x_axis+"%20&api_key=eca1902cb724e40fdb20fd628b47489b15134d79", function(data) {
+      
+      // Refactorizar para no hacerlo en cada gráfica
+      var _data = data;
+      if(domain === undefined){
+        _data['min_domain'] = d3.min(_data.rows, function(data){return data.date_processed;})
+        _data['max_domain'] = d3.max(_data.rows, function(data){return data.date_processed;})
+        _domain = [new Date(_data.min_domain),new Date(_data.max_domain)];
+      } else {
+        _domain = domain;
+      }
+
+      var x_col = subject[index].x_axis;
+      var x_scale = d3.scale.linear()
+        .range([margin,w-margin])
+        .domain(_domain);
+
+      var previous_column;
+
+      for (var i = 0; i < subject[index].series.length; i++) {
+
+        var g = svg[index].append("svg:g");
+        var strokeColor = subject[index].series[i].strokeColor;
+        var y_col = subject[index].series[i].column;
+        var y_col_name = subject[index].series[i].name;
+
+        var y_scale = d3.scale.linear()
+          .range([h-margin, top_margin]) // Esto está mal
+          .domain(subject[index].series[i].y_extent);
+          //.domain([0,1000]);
+
+        // remove null values
+        var data_col = _data.rows.filter(function(d) {
+          return (d[y_col] != null) && (d[y_col] != 0)
+        });
+
+        var area = d3.svg.area()
+            
+            .x(function(d) {return x_scale(new Date(d[x_col]))})
+            .y0(function(d) {                             
+              var value = y_scale(0);
+              if (i>0) {
+                value = y_scale(d[previous_column]);
+              }
+              return value;
+            })
+            .y1(function(d) {              
+              var value = y_scale(d[y_col]);
+              if (i>0) {
+                value = y_scale(d[y_col]+d[previous_column])
+              };
+              return value;
+            });
+      
+        g.append("svg:path")
+          .attr("d", area(data_col))          
+          .attr("style",'stroke:'+strokeColor)
+          .attr("style",'fill:'+strokeColor)
+
+        g.selectAll("circle")
+          .data(data_col)
+          .enter()
+          .append("circle")
+          .attr("class", 'linedot linedot'+i)
+          .attr("style",function(d){ var _fill = (new Date(d.date_processed).getMonth() + 1 == 1) ? 'fill:'+strokeColor : 'display: none'; return _fill;})
+          .attr("cx", function(d){return x_scale(new Date(d[x_col]))})
+          .attr("cy", function(d){
+            var value = y_scale(d[y_col]);
+            if (i>0) {
+              value = y_scale(d[y_col]+d[previous_column])
+            };
+            return value;
+          })
+          .attr("r", LINE_DOT_R)
+          .attr("name", function(d){return d[y_col]}) //Uses this for tooltip
+          .on("mouseover", function(d) {
+            tooltip.style("visibility", "visible")
+              .text($(this).attr('name'))
+              .style("top", $(this).offset().top+30+"px")
+              .style("left", $(this).offset().left-25+"px");
+          })
+          .on("mouseout", function(){return tooltip.style("visibility", "hidden");});
+          console.log("O");
+        previous_column = y_col; // Para que el siguiente área tenga el dato de partida
+
+      }
+    
+    }
+
+    );
+
+  }
 
   //Draws a lineChart
   //index:index of the table on the json, 
