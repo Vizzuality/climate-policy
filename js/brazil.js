615e3860e351abf6a50400e52fddf98d323c94b5
Diego Cano <diegocano@Diegos-MacBook-Pro.local>
2013-04-04 20:20:00 +0200

    New version of bars graph

--- js/brazil.js
+++ js/brazil.js
@@ -57,7 +57,141 @@ $(document).ready(function() {
     drawChart(1, std_domain);
     drawChart(2, std_domain);
     drawChart(3, std_domain);
+    drawBarChart(4, std_domain);
   })
+
+
+  function drawBarChart(index,domain) {
+
+    d3.json('http://cpi.cartodb.com/api/v2/sql?q=SELECT%20*%20FROM%20'+subject[index].table+"%20&api_key=eca1902cb724e40fdb20fd628b47489b15134d79", function(data) {
+      console.log(data);
+
+      // Calculo el domain y range en general para todos los valores            
+      var negat = [];
+      var posit = [];
+
+      data.rows.forEach(function(d){
+        //console.log(d);
+        var values = [];
+        for (var j = 0; j < subject[index].x_groups.length; j++) {
+          var value = d[subject[index].x_groups[j].column];
+          if (value > 0) {
+            posit.push(value);
+          } else {
+            negat.push(Math.abs(value));
+          }          
+        }
+      });
+      
+      var max_negat  = d3.max(negat);
+      var max_posit = d3.max(posit);
+      var max = d3.max([max_negat, max_posit]);
+
+      console.log("*"+max_negat);
+      console.log("*"+max_posit);
+      console.log("*"+max);
+
+      var group_width = 650/subject[index].x_groups.length;
+      var series_step = 200/data.rows.length;
+      var bar_height = 100/data.rows.length;
+      //var max_bar = group_width*(max/(max_negat+max_posit));
+      console.log(">"+parseFloat(max));
+      console.log(">>"+parseFloat(parseFloat(max_negat) + parseFloat(max_posit)));
+      console.log(">>>"+parseFloat(max)/parseFloat(parseFloat(max_negat) + parseFloat(max_posit)));
+      var max_bar = group_width*parseFloat(max)/parseFloat(parseFloat(max_negat) + parseFloat(max_posit));
+      console.log(max_bar);
+
+      var bar_width_scale = d3.scale.linear()
+        .domain([0,max])
+        .range([0, max_bar]); // La máxima anchura es la total por la rel del máximo con 
+
+      var zero_pos = bar_width_scale(max_negat); // La posición del cero es igual a la anchura del máximo mínimo
+      console.log("zero_pos: "+zero_pos);
+
+      // Series labels
+      svg[index].selectAll("text.graph-series-label")
+      .data(data.rows)
+      .enter()
+      .append("text")
+      .attr("class","graph-series-label")
+      .text(function(d) {
+        return d.variable;
+      })
+      .attr("x", function(d,i) {
+        return 40;
+      })
+      .attr("y", function(d,i) {
+        return 170 + (i * series_step);
+      });
+
+      // Group labels
+      svg[index].selectAll("text.graph-groups-label")
+        .data(subject[index].x_groups)
+        .enter()
+        .append("text")
+        .attr("class","graph-groups-label")
+        .text(function(d) {
+          return d.label;
+        })
+        .attr("x", function(d,i) {
+          return 363 + group_width*i + zero_pos - 40;
+        })
+        .attr("y", function(d,i) {
+          return 140;
+        });
+      
+      // Recorro los x-groups para cada serie
+      for (var j = 0; j < subject[index].x_groups.length; j++) {
+
+        var group_name = subject[index].x_groups[j].column;
+
+        svg[index].selectAll("rect."+group_name)
+        .data(data.rows)
+        .enter()
+        .append("rect")
+        .attr("class",group_name)
+        .attr("x", function(d,i) {
+          if (d[group_name] > 0) {
+            return 363 + group_width*j + zero_pos;
+          } else {
+            return 363 + group_width*j + zero_pos - bar_width_scale(Math.abs(d[group_name]));
+          }
+        })
+        .attr("y", function(d,i) {
+          return 163 + (i * series_step) - bar_height/2;
+        })
+        .attr("width", function(d,i) {
+          var bar_width = bar_width_scale(Math.abs(d[group_name]));
+          if (bar_width < 1) bar_width = 2;
+          return bar_width;
+        })
+        .attr("height", function(d,i) {
+          return bar_height;
+        })
+        .attr("style",function (d) {
+          return "fill: #546DBC";
+        })
+        .attr("name", function(d){
+          return Math.round(d[group_name]*1000)/1000;
+        }) //Uses this for tooltip
+        .on("mouseover", function(d) {
+          d3.selectAll(".overlay-line").style("visibility", "visible");
+          tooltip.style("visibility", "visible")
+            .text($(this).attr('name'))
+            .style("top", $(this).offset().top+30+"px")
+            .style("left", $(this).offset().left-25+"px");          
+        })
+        .on("mousemove", mousemove)
+        .on("mouseout", function(){
+          d3.selectAll(".overlay-line").style("visibility", "hidden");
+            tooltip.style("visibility", "hidden");
+          });
+        
+      }
+  
+    });
+  }
+
 
   //Draws a lineChart
   //index:index of the table on the json, 
@@ -123,7 +257,6 @@ $(document).ready(function() {
             .attr("name", function(d){return d[y_col]}) //Uses this for tooltip
             .on("mouseover", function(d) {
               d3.selectAll(".overlay-line").style("visibility", "visible");
-
               tooltip.style("visibility", "visible")
                 .text($(this).attr('name'))
                 .style("top", $(this).offset().top+30+"px")
@@ -132,7 +265,6 @@ $(document).ready(function() {
             .on("mousemove", mousemove)
             .on("mouseout", function(){
               d3.selectAll(".overlay-line").style("visibility", "hidden");
-
               tooltip.style("visibility", "hidden");
             });
 
