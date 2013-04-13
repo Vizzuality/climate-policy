6cf473c276a941b5f4e34404465a5c34d2f4f807
Carlos Matallín <matallo@vizzuality.com>
2013-04-13 22:42:39 +0200

    add regions.css to precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,7 @@ ClimatePolicy::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( ie.css regions.js )
+  config.assets.precompile += %w( ie.css home.css regions.css regions.js )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
